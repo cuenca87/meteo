cccf10da9e41f1b7b1368f50d687f5ab301c030c
cuenca87 <cuenca87@gmail.com>
2026-07-09 13:24:41 +0200

    Estaciones: barra Lista/Mapa fuera del contenido para que no desaparezca

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meteo-v10";
+const CACHE = "meteo-v11";
 const PRECACHE = ["/", "/index.html", "/icon.svg", "/manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
