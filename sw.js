0ae5fb374ab2595d9a321ffad942a55e75b78712
cuenca87 <cuenca87@gmail.com>
2026-08-20 11:26:35 +0200

    Popup de resumen del día también en Previsión (Weather Company y Open-Meteo)
    Reutiliza el modal ya creado para la pestaña AEMET. Weather Company ya trae
    frases narrativas en español en su API (dp.narrative), así que se muestran
    directamente; Open-Meteo no tiene texto, así que se sintetiza igual que se
    hizo para AEMET a partir de las franjas horarias. El popup también incluye
    la franja horaria y las estadísticas del día.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meteo-v15";
+const CACHE = "meteo-v16";
 const PRECACHE = ["./", "./index.html", "./icon.svg", "./manifest.json"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
